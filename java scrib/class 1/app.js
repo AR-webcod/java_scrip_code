ec07d1ff78abc16e384e69d54f0fddf394efca2f
ABDUL REHMAN <abdulrehmanars1996@gmail.com>
2026-03-13 00:12:26 +0500

    Add files via upload
    practice

---
+++ java scrib/class 1/app.js
@@ -0,0 +1,8 @@
+// document.write(30+50)
+// document.write("welcome AR")
+// console.log("welcome to ar")
+// console.log(10+20)
+// alert("welcome")
+
+
+
---
+++ java scrib/variable/app.js
@@ -0,0 +1,45 @@
+// var n= 20
+// var m= 50
+// console.log(n+m)
+// console.log(n-m)
+// console.log(n*m)
+// console.log(n/m)
+
+// var n= 60
+// console.log(n+m)      // var value change kar sakta hai magar keyword fixed huta hai
+
+// var username = "welcome to AR"
+// console.log(username) 
+// console.log(username) 
+// console.log(username) 
+
+// username = "saad"
+// console.log(username )
+
+// let a=10
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// a=50
+// console.log(a)
+
+// {
+//     let a = 10
+//     console.log(a)    //this is block scoped variable
+
+// }
+
+// let a;
+//     {
+//         a = 10         //this is not block scoped variable but print in console.log
+
+//     }
+// console.log(a)    
+
+
+// const n=10
+// console.log(n)
+                            
+// n=50                    // const mein value change nahi hu sakta
+// console.log(n);     
