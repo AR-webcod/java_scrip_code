38f1f248593d1370d9ab6ab3f3968dde0a304e6d
ABDUL REHMAN <abdulrehmanars1996@gmail.com>
2026-03-15 22:34:39 +0500

    updated
    updated

---
+++ primitive data tupes/app.js
@@ -0,0 +1,58 @@
+// let a=12                            this prativive data types
+// console.log(a,typeof(a));
+
+// let n                               this undifined data types
+// console.log(n,typeof(n));
+
+// boolen data types      
+// let status=true                       //ture ki mathemathic value  1 quant huta hai
+// console.log(status);
+
+// let status=false                     //false ki mathemathic value 0 quant huta hai
+// console.log(status)
+
+// let n=null
+// console.log(n);                        // null ek object data types hai //
+
+
+
+////////ARRAY///////////
+
+// let l = [10,20,30,40,99]
+// console.log(l)
+
+// let obj={
+//    name:"abdul rehman",
+//    cname:"ar"
+// }
+// console.log(obj);
+
+// let n=10
+// console.log(n);
+
+// let a=20
+// console.log(a*"20");
+
+// let b=6
+// console.log(b+"6");        // agar string ke ansdar number hai tu conkaet kare ga
+
+// let b=6                       //  answere  NAN= not a number
+// console.log(6+"ar");        
+
+// let num1=prompt("enter the birthday")     // ye hamesha value string me ata hai
+// console.log(num1);
+
+// let num1 = parseInt(prompt("enter the value"))       // ye string se convert huke nummber me hu jaye ga,magar float value ko hata de ga.float value .ke baad ke number ko hata de ga.
+// console.log(num1);
+
+
+// let num1= parseFloat(prompt("enta the value"))      // ye float value ko convert nahi kare, tpyeof number hi hu ga.
+// console.log(num1);
+
+// let num2= Number(prompt("enter the value"))           // agar float value tu bhe answere same hu ga or agar numbere hu to sam hu ga
+// console.log(num2);
+
+
+
+
+
